168dec6819190bc1ca320aecf273753d6393ddf1
ROOBALJINDAL <roobaljindal9@gmail.com>
2017-09-07 11:44:27 +0530

    Security enabled: In memory authentication

--- src/main/java/com/testing/demo/DemoApplication.java
+++ src/main/java/com/testing/demo/DemoApplication.java
@@ -1,15 +1,21 @@
 package com.testing.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 @SpringBootApplication(exclude=SecurityAutoConfiguration.class)
-public class DemoApplication {
+public class DemoApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
 }
--- src/main/java/com/testing/demo/LoginController.java
+++ src/main/java/com/testing/demo/LoginController.java
@@ -3,6 +3,7 @@ package com.testing.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
